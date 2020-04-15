0a15eefdaf1e714e9f6eec1236a81122a3f34a9e
Aviral Goel <goel.aviral@gmail.com>
2020-04-14 20:19:49 -0400

    Switch to C++

---
+++ src/init.cpp
@@ -0,0 +1,31 @@
+#define R_NO_REMAP
+#include "inject.hpp"
+
+#include <R.h>
+#include <R_ext/Rdynload.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+
+extern "C" {
+
+static const R_CallMethodDef callMethods[] = {
+    {"get_type_check_function", (DL_FUNC) &get_type_check_function, 0},
+    {"get_type_check_function_wrapper",
+     (DL_FUNC) &get_type_check_function_wrapper,
+     0},
+    {"set_type_check_function", (DL_FUNC) &set_type_check_function, 2},
+    {"reset_type_check_function", (DL_FUNC) &reset_type_check_function, 0},
+    {"inject_type_check", (DL_FUNC) &inject_type_check, 4},
+    {"check_type", (DL_FUNC) &check_type, 5},
+    {"environment_name", (DL_FUNC) &environment_name, 1},
+    {NULL, NULL, 0}};
+
+void R_init_contractR(DllInfo* dll) {
+    R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+
+    R_DotCallSym = Rf_install(".Call");
+    reset_type_check_function();
+}
+
+}
---
+++ src/inject.hpp
@@ -0,0 +1,18 @@
+#include <R.h>
+#include <Rinternals.h>
+
+extern SEXP R_DotCallSym;
+
+extern "C" {
+
+/* .check_call calls */
+
+SEXP get_type_check_function();
+SEXP get_type_check_function_wrapper();
+SEXP set_type_check_function(SEXP, SEXP);
+SEXP reset_type_check_function();
+SEXP inject_type_check(SEXP, SEXP, SEXP, SEXP);
+SEXP check_type(SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP environment_name(SEXP);
+
+}
