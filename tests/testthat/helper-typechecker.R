a8da8ed8f7106fd9e6d77bc5e7ba6c095bde821b
Aviral Goel <goel.aviral@gmail.com>
2020-04-18 20:31:58 -0400

    Add typechecker testing utilities and tests for type double

---
+++ tests/testthat/helper-typechecker.R
@@ -0,0 +1,132 @@
+library(purrr)
+library(stringr)
+library(rlang)
+library(magrittr)
+
+expect_typechecking_warnings <- function(object, ..., specs) {
+    pattern <- paste0("expected:", "\\w+", expected, "\n", ".*", "actual:", "\\w+", actual)
+    expect_warning(object(...), pattern)
+}
+
+typecheck <- function(object, pkg_name = ".GlobalEnv") {
+    list(fun = object,
+         pkg_name = pkg_name,
+         name = as.character(substitute(object)),
+         call_expression = "<undefined>",
+         warnings = character(0))
+}
+
+apply_arguments <- function(object, ...) {
+    object$warnings <- capture_warnings(object$fun(...))
+
+    arguments <- list(...)
+    call_list <- c(as.symbol(object$name), arguments)
+    object$call_expression <- deparse(as.call(call_list))
+
+    object
+}
+
+expect_contract_violations <- function(object, expected_count) {
+    actual_count <- length(object$warnings)
+    if (expected_count != actual_count) {
+        fail(message = str_glue("`{call_expression}` produced {actual_count} warnings, {expected_count} expected",
+                                call_expression = object$call_expression,
+                                actual_count = actual_count,
+                                expected_count = expected_count))
+    }
+
+    object
+}
+
+expect_arg_contract_violation <- function(object, index, expected_type, actual_type) {
+    parameter_name <- names(formals(object$fun))[index]
+    patterns <- c(
+        str_glue("parameter\\s+\\Q'{parameter_name}'\\E", parameter_name = parameter_name),
+        str_glue("position\\s+\\Q{index}\\E", index = index),
+        str_glue("expected.*\\Q{expected_type}\\E\\n", expected_type = expected_type),
+        str_glue("actual.*\\Q{actual_type}\\E$", actual_type = actual_type)
+    )
+
+    warning_message <- object$warnings[index]
+
+    result <-
+      patterns %>%
+      map_lgl(function(pattern) str_detect(warning_message, regex(pattern)))
+
+    if (!all(result)) {
+        expected_pattern <- (patterns[!result])[1]
+        error_message <- str_glue("`{call_expression}` produced unexpected warnings.",
+                                  "Expected match: {expected_pattern}",
+                                  "Actual values:",
+                                  "* {warning_message}",
+                                  .sep = "\n",
+                                  call_expression = object$call_expression,
+                                  expected_pattern = expected_pattern,
+                                  warning_message = warning_message)
+
+        fail(message = error_message)
+    }
+
+    succeed()
+    object
+}
+
+
+expect_ret_contract_violation <- function(object, expected_type, actual_type) {
+    patterns <- c(
+        str_glue("return value"),
+        str_glue("expected.*\\Q{expected_type}\\E\\n", expected_type = expected_type),
+        str_glue("actual.*\\Q{actual_type}\\E$", actual_type = actual_type)
+    )
+
+    warning_message <- tail(object$warnings, n = 1)
+
+    result <-
+      patterns %>%
+      map_lgl(function(pattern) str_detect(warning_message, regex(pattern)))
+
+    if (!all(result)) {
+        expected_pattern <- (patterns[!result])[1]
+        error_message <- str_glue("`{call_expression}` produced unexpected warnings.",
+                                  "Expected match: {expected_pattern}",
+                                  "Actual values:",
+                                  "* {warning_message}",
+                                  .sep = "\n",
+                                  call_expression = object$call_expression,
+                                  expected_pattern = expected_pattern,
+                                  warning_message = warning_message)
+
+        fail(message = error_message)
+    }
+
+    succeed()
+    object
+}
+
+create_s4 <- function() {
+    setClass("Person", representation(name = "character", age = "numeric"))
+    john_doe <- new("Person", name = "John Doe", age = 30)
+}
+
+create_weakref <- function() {
+    new_weakref(new.env())
+}
+
+create_list <- function(n) {
+    stopifnot(n > 5)
+    as.list(character(n))
+}
+
+create_tuple <- function(n) {
+    stopifnot(n <= 5)
+    as.list(character(n))
+}
+
+create_struct <- function(n) {
+    element_names <- replicate(n, "name")
+    struct <- as.list(replicate(n, 3))
+    names(struct) <- element_names
+    struct
+}
+
+na_logical_ <- as.logical(NA_integer_)
---
+++ tests/testthat/test-typechecker.R
@@ -0,0 +1,326 @@
+test_that("contract for `double` is enforced correctly", {
+
+    fun <- function(x) if (is_missing(x)) 3L else x
+
+    inject_type_check_call(fun, "<double> => double", pkg_name = ".GlobalEnv")
+
+################################################################################
+##    double
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(2.0) %>%
+      expect_contract_violations(0)
+
+################################################################################
+## integer
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(integer()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "integer[]") %>%
+      expect_ret_contract_violation("double", "integer[]")
+
+    typecheck(fun) %>%
+      apply_arguments(2L) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "integer") %>%
+      expect_ret_contract_violation("double", "integer")
+
+    typecheck(fun) %>%
+      apply_arguments(c(2L, 3L)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "integer[]") %>%
+      expect_ret_contract_violation("double", "integer[]")
+
+    typecheck(fun) %>%
+      apply_arguments(NA_integer_) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^integer") %>%
+      expect_ret_contract_violation("double", "^integer")
+
+    typecheck(fun) %>%
+      apply_arguments(c(32L, NA_integer_)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^integer[]") %>%
+      expect_ret_contract_violation("double", "^integer[]")
+
+################################################################################
+## character
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(character()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "character[]") %>%
+      expect_ret_contract_violation("double", "character[]")
+
+    typecheck(fun) %>%
+      apply_arguments("2") %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "character") %>%
+      expect_ret_contract_violation("double", "character")
+
+    typecheck(fun) %>%
+      apply_arguments(c("2", "3")) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "character[]") %>%
+      expect_ret_contract_violation("double", "character[]")
+
+    typecheck(fun) %>%
+      apply_arguments(NA_character_) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^character") %>%
+      expect_ret_contract_violation("double", "^character")
+
+    typecheck(fun) %>%
+      apply_arguments(c("32", NA_character_)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^character[]") %>%
+      expect_ret_contract_violation("double", "^character[]")
+
+################################################################################
+## complex
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(complex()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "complex[]") %>%
+      expect_ret_contract_violation("double", "complex[]")
+
+    typecheck(fun) %>%
+      apply_arguments(2+2i) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "complex") %>%
+      expect_ret_contract_violation("double", "complex")
+
+    typecheck(fun) %>%
+      apply_arguments(c(2+1i, 3+3i)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "complex[]") %>%
+      expect_ret_contract_violation("double", "complex[]")
+
+    typecheck(fun) %>%
+      apply_arguments(NA_complex_) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^complex") %>%
+      expect_ret_contract_violation("double", "^complex")
+
+    typecheck(fun) %>%
+      apply_arguments(c(3 + 2i, NA_complex_)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^complex[]") %>%
+      expect_ret_contract_violation("double", "^complex[]")
+
+################################################################################
+## logical
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(logical()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "logical[]") %>%
+      expect_ret_contract_violation("double", "logical[]")
+
+    typecheck(fun) %>%
+      apply_arguments(TRUE) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "logical") %>%
+      expect_ret_contract_violation("double", "logical")
+
+    typecheck(fun) %>%
+      apply_arguments(c(TRUE, FALSE)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "logical[]") %>%
+      expect_ret_contract_violation("double", "logical[]")
+
+    typecheck(fun) %>%
+      apply_arguments(na_logical_) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^logical") %>%
+      expect_ret_contract_violation("double", "^logical")
+
+    typecheck(fun) %>%
+      apply_arguments(c(TRUE, na_logical_)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "^logical[]") %>%
+      expect_ret_contract_violation("double", "^logical[]")
+
+################################################################################
+## raw
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(raw()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "raw[]") %>%
+      expect_ret_contract_violation("double", "raw[]")
+
+    typecheck(fun) %>%
+      apply_arguments(as.raw(40)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "raw") %>%
+      expect_ret_contract_violation("double", "raw")
+
+    typecheck(fun) %>%
+      apply_arguments(as.raw(c(32, 45))) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "raw[]") %>%
+      expect_ret_contract_violation("double", "raw[]")
+
+################################################################################
+## environment
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(new.env()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "environment") %>%
+      expect_ret_contract_violation("double", "environment")
+
+################################################################################
+## expression
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(expression(1 + 2)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "expression") %>%
+      expect_ret_contract_violation("double", "expression")
+
+################################################################################
+## language
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(quote(1 + 2)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "language") %>%
+      expect_ret_contract_violation("double", "language")
+
+################################################################################
+## symbol
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(as.symbol("x")) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "symbol") %>%
+      expect_ret_contract_violation("double", "symbol")
+
+################################################################################
+## pairlist
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(pairlist(1, 2, 3)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "pairlist") %>%
+      expect_ret_contract_violation("double", "pairlist")
+
+################################################################################
+## s4
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(create_s4()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "s4") %>%
+      expect_ret_contract_violation("double", "s4")
+
+################################################################################
+## weakref
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(create_weakref()) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "weakref") %>%
+      expect_ret_contract_violation("double", "weakref")
+
+################################################################################
+## missing
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments() %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "???") %>%
+      expect_ret_contract_violation("double", "integer")
+
+
+################################################################################
+## null
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(NULL) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "null") %>%
+      expect_ret_contract_violation("double", "null")
+
+################################################################################
+## list
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(create_list(6)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "list<character>") %>%
+      expect_ret_contract_violation("double", "list<character>")
+
+################################################################################
+## tuple
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(create_tuple(2)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "tuple<character, character>") %>%
+      expect_ret_contract_violation("double", "tuple<character, character>")
+
+################################################################################
+## struct
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(create_struct(2)) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "struct<`name`: double, `name`: double>") %>%
+      expect_ret_contract_violation("double", "struct<`name`: double, `name`: double>")
+
+################################################################################
+## closure
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(function(x, y) {
+          x + y
+      }) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "any => any") %>%
+      expect_ret_contract_violation("double", "any => any")
+
+################################################################################
+## builtin
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(max) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "any => any") %>%
+      expect_ret_contract_violation("double", "any => any")
+
+################################################################################
+## special
+################################################################################
+
+    typecheck(fun) %>%
+      apply_arguments(`if`) %>%
+      expect_contract_violations(2) %>%
+      expect_arg_contract_violation(1, "double", "any => any") %>%
+      expect_ret_contract_violation("double", "any => any")
+
+})
