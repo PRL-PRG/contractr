ca21ca1e2b2f123d3e6a38a51f82f601f95e4343
Aviral Goel <goel.aviral@gmail.com>
2020-04-24 10:09:02 -0400

    Add R severity API

---
+++ R/severity.R
@@ -0,0 +1,20 @@
+#' @export
+set_severity <- function(severity) {
+    if (missing(severity)) {
+        severity <- getOption("contractr.severity")
+        if (is.null(severity)) {
+            severity <- "warning"
+        }
+    }
+
+    if (!(severity %in% c("warning", "error", "silent"))) {
+        stop("severity should be 'warning', 'error' or 'silent'; not '", severity, "'")
+    }
+
+    invisible(.Call(C_set_severity, severity))
+}
+
+#' @export
+get_severity <- function() {
+    .Call(C_get_severity)
+}
