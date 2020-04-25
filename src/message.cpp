a61161f07fd65df2a904cf6a5a8dc7f6b469f538
Aviral Goel <goel.aviral@gmail.com>
2020-04-24 21:00:17 -0400

    Escape input string to prevent printf from expanding format specifiers

--- src/message.cpp
+++ src/message.cpp
@@ -7,6 +7,22 @@
 const int MESSAGE_BUFFER_SIZE = 10000;
 char buffer[MESSAGE_BUFFER_SIZE];
 
+std::string escaped_buffer;
+
+void escape_printf_specifiers() {
+    int i = 0;
+    while (buffer[i] != '\0') {
+        char letter = buffer[i];
+        if (letter == '%') {
+            escaped_buffer.append("%%");
+        } else {
+            escaped_buffer.push_back(letter);
+        }
+        ++i;
+    }
+    escaped_buffer.push_back('\0');
+}
+
 void show_message(const char* format, ...) {
     Severity severity = get_severity();
     if (severity == Severity::Silent) {
@@ -23,13 +39,15 @@ void show_message(const char* format, ...) {
     vsnprintf(buffer, MESSAGE_BUFFER_SIZE, format, vlist);
     va_end(vlist);
 
+    escape_printf_specifiers();
+
     if (severity == Severity::Warning) {
-        warningcall(R_NilValue, buffer);
+        warningcall(R_NilValue, escaped_buffer.c_str());
         return;
     }
 
     if (severity == Severity::Error) {
-        errorcall(R_NilValue, buffer);
+        errorcall(R_NilValue, escaped_buffer.c_str());
         return;
     }
 }
