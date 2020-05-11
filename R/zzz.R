6cf7b50c55cff40248e283621b74cf4f116fdb5d
Aviral Goel <goel.aviral@gmail.com>
2020-05-11 15:48:29 +0000

    Guard against contract insertion in non-package packages

--- R/zzz.R
+++ R/zzz.R
@@ -6,14 +6,25 @@
 
     handle_package <- function(package_name, ...) {
         if (should_autoinject()) {
-            fun_names <- insert_package_contract(package_name)
+            tryCatch(
+                fun_names <- insert_package_contract(package_name),
+                error = function(e) {
+                    expected_message <- sprintf("there is no package called '%s'", package_name)
+                    if(length(grep(expected_message, e$message)) == 1) {
+                        message(expected_message)
+                    }
+                    else {
+                        stop(e)
+                    }
+                }
+            )
             if (length(fun_names) != 0) {
                 message("Added contract to ", length(fun_names), " ", package_name, " function(s)")
             }
         }
     }
 
-    remove_packages <- c(".GlobalEnv", "Autoloads", "tools:callr")
+    remove_packages <- c(".GlobalEnv", "Autoloads", "tools:callr", "tools:rstudio")
     loaded_packages <- setdiff(remove_package_prefix(search()), remove_packages)
     installed_packages <- installed.packages()[, 1]
     remaining_packages <- setdiff(installed_packages, loaded_packages)
